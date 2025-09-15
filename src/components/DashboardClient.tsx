5313a06fefb8cd0a1ab728ef4e5e8b738d0f5625
Abilitix <admin@abilitix.com.au>
2025-09-16 00:36:57 +1000

    Fix JSX syntax error in DashboardClient
    - Remove extra closing div tag that was causing build failure
    - Clean up JSX structure after removing duplicate logo and tenant display"

--- src/components/DashboardClient.tsx
+++ src/components/DashboardClient.tsx
@@ -57,7 +57,6 @@ export default function DashboardClient({ user }: DashboardClientProps) {
                 </span>
               )}
             </div>
-          </div>
         </div>
       </section>
 

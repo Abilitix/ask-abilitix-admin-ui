7f0dd0974871aacc2ee7667d810a947f7a76cf7d
Abilitix <admin@abilitix.com.au>
2025-09-17 18:24:42 +1000

    Fix TopNav prop error in admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,7 +8,7 @@ export default async function AdminLayout({ children }: { children: React.ReactN
   
   return (
     <>
-      <TopNav userRole={user.role} />
+      <TopNav />
       <main className="mx-auto max-w-6xl px-4 py-8">
         {children}
       </main>
